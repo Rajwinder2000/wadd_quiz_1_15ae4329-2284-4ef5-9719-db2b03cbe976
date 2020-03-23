c8d0d0c267c47187df544d0b41eaa5d276de44e4
Rajwinder2000 <61202133+Rajwinder2000@users.noreply.github.com>
2020-03-23 12:45:01 -0700

    Create problem_two.js

---
+++ problem_two.js
@@ -0,0 +1,8 @@
+var message="informtion technology";
+var acronym=getAcronym(message);
+function getAcronym(message){
+  var words = message.split;
+  var acr = words.map;
+  return acr.join('').toUpperCase( );
+  
+};
